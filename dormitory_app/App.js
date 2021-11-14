32ef11f46ef35e88f776f6fb7f2042b7664cce5c
macsakarn <macsakarn@gmail.com>
2021-11-15 03:58:08 +0700

    Start!!

---
+++ dormitory_app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Text>this is main design</Text>
+      </>
+    );
+  }
+}
+
+export default App;
