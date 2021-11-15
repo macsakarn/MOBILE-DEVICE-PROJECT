45a1fe46da06b47835689360d1fe53c8868ab8f6
macsakarn <macsakarn@gmail.com>
2021-11-15 20:24:07 +0700

    start

---
+++ dormitory_admin/app/components/Tabbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Tabbar = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Tabbar
+
+const styles = StyleSheet.create({})
