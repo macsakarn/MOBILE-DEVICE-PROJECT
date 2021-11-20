bb5b4d241047107fce73ce123bfb3fc9c9ce9990
bankjirayuth <53044682+bankjirayuth@users.noreply.github.com>
2021-11-20 16:47:32 +0700

    เหลือ UiID ห้อง

--- dormitory_admin/src/screen/textbox/showAnnounce.js
+++ dormitory_admin/src/screen/textbox/showAnnounce.js
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
 import Colors from '../../assets/color';
 
 export default showAnnounce = (props) => {
